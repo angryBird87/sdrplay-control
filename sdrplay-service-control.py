9080b5d948531c017bc7707be4b11d1d3638aadb
Thomas Koller-Cherek <thomas.koller@htlpinkafeld.at>
2023-04-08 13:06:54 +0200

    Initial commit

---
+++ sdrplay-service-control.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import sys
+import subprocess
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+def restart():
+    subprocess.run("sudo systemctl restart sdrplay", shell=True)
+
+def stop():
+    subprocess.run("sudo systemctl stop sdrplay", shell=True)
+
+app = QApplication([])
+window = QWidget()
+window.resize(240, 240)
+window.setWindowTitle("SDRPlay-Treiber-Management")
+
+layout = QVBoxLayout()
+restartButton = QPushButton("Neustarten")
+restartButton.clicked.connect(restart)
+layout.addWidget(restartButton)
+
+stopButton = QPushButton("Stoppen")
+stopButton.clicked.connect(stop)
+layout.addWidget(stopButton)
+
+window.setLayout(layout)
+
+window.show()
+
+sys.exit(app.exec())
